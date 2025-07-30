8fbff2998e3fb53b1de0d4b1808e5a1a11378574
siddhant0022 <148846164+siddhant0022@users.noreply.github.com>
2025-07-30 20:28:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { SplineScene } from "@/components/ui/splite";
+import { SplineSceneBasic } from './components/heroSection'
+import { Card } from './components/ui/card'
+import EtherealBeamsHero from './components/ui/external-beams-hero'
+import { Spotlight } from "@/components/ui/spotlight"
+ 
+
+
+function App() {
+  return (
+    
+      <div>
+    <EtherealBeamsHero/>
+      
+    <SplineSceneBasic/>
+   
+
+
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/components/hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EtherealBeamsHero from './ui/external-beams-hero'
+
+function hero() {
+  return (
+  <div>
+    <EtherealBeamsHero/>
+  </div>
+  )
+}
+
+export default hero
